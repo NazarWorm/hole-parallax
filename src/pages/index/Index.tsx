84f57e14036eed24d0bf2548daa50567085034e7
NazarWorm <tigrana334@gmail.com>
2023-06-05 09:36:15 +0300

    Installed Tailwind

---
+++ src/pages/index/Index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface IIndex{
+}
+
+function Index ( props: IIndex ){
+
+   return (
+      <>
+         <div className=''>
+            <span> Index Page </span>
+         </div>
+      </>
+   );
+}
+
+export default Index
